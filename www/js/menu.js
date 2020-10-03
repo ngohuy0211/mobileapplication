29bf9abcc322074ad40bfd9aa582455331cb6aae
PhanNgocHoang <hoangpngch17194@fpt.edu.vn>
2020-10-03 10:45:01 +0700

    update 1

---
+++ www/js/menu.js
@@ -0,0 +1,29 @@
+// $(document).ready(function (){
+//     $('#search').on('tap', () => {
+//         const content = $('#content')
+//         const newContent = `
+//                 <form>
+//                          <label for="search-1">Search:</label>
+//                          <input type="search" name="search-1" id="search-1" value="">
+//                     </form>
+//                 <ul data-role="listview" data-inset="true">
+//                     <li><a href="#">
+//                         <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" class="img">
+//                     <h2>Broken Bells</h2>
+//                     <p>Broken Bells</p></a>
+//                     </li>
+//                     <li><a href="#">
+//                         <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" class="img">
+//                     <h2>Warning</h2>
+//                     <p>Hot Chip</p></a>
+//                     </li>
+//                     <li><a href="#">
+//                         <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" class="img">
+//                     <h2>Wolfgang Amadeus Phoenix</h2>
+//                     <p>Phoenix</p></a>
+//                     </li>
+//                 </ul>`
+//         content.empty().append(newContent)
+//     })
+
+// })
