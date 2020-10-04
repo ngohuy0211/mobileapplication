766ac867ebe2bcd5a7179ded15eafc32305025d6
PhanNgocHoang <hoangpngch17194@fpt.edu.vn>
2020-10-04 09:33:23 +0700

    test

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,29 +1,36 @@
-// $(document).ready(function (){
-//     $('#search').on('tap', () => {
-//         const content = $('#content')
-//         const newContent = `
-//                 <form>
-//                          <label for="search-1">Search:</label>
-//                          <input type="search" name="search-1" id="search-1" value="">
-//                     </form>
-//                 <ul data-role="listview" data-inset="true">
-//                     <li><a href="#">
-//                         <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" class="img">
-//                     <h2>Broken Bells</h2>
-//                     <p>Broken Bells</p></a>
-//                     </li>
-//                     <li><a href="#">
-//                         <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" class="img">
-//                     <h2>Warning</h2>
-//                     <p>Hot Chip</p></a>
-//                     </li>
-//                     <li><a href="#">
-//                         <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" class="img">
-//                     <h2>Wolfgang Amadeus Phoenix</h2>
-//                     <p>Phoenix</p></a>
-//                     </li>
-//                 </ul>`
-//         content.empty().append(newContent)
-//     })
+$(document).ready(function (){
+    $('#show').on('click', () => {
+        const content = $('#result').listview()
+            const newContent = `
+                        <li>
+                        <div class="w3-card-4" style="width:50%">
+                        <a href="#">
+                            <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" style="width:100%">
+                            <div class="w3-container w3-center">
+                            <h2>Broken Bells</h2>
+                            <p>Broken Bells</p>
+                            </div
+                        </a>
+                        </div>
+                        </li>
+                        <li>
+                        <div class="w3-card-4" style="width:50%">
+                        <a href="#">
+                            <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" style="width:100%">
+                        <h2>Warning</h2>
+                        <p>Hot Chip</p></a>
+                        </div>
+                        </li>
+                        <li>
+                        <div class="w3-card-4" style="width:50%">
+                        <a href="#">
+                            <img src="https://images.alphacoders.com/943/thumb-1920-943148.jpg" style="width:100%">
+                        <h2>Wolfgang Amadeus Phoenix</h2>
+                        <p>Phoenix</p></a>
+                        </div>
+                        </li>
+                `
+        content.append(newContent)
+        })
 
-// })
+})
