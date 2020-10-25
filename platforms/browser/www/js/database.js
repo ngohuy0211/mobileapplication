c8c6f5ed7947b916e3ae2878c040f70a1841c230
Ngo Quang Huy <huynqgch17251@fpt.edu.vn>
2020-10-25 14:22:45 +0700

    fix data

--- platforms/browser/www/js/database.js
+++ platforms/browser/www/js/database.js
@@ -50,32 +50,25 @@ request.onsuccess = function(event) {
     console.log("Successfull!: "+ database);
 };
 function getAllData(collectionName) {
-    console.log(database)
-    // const objectStore = transaction.objectStore(collectionName)
-    // request = objectStore.getAll();
-    // return request
+    return database.transaction([collectionName], "readonly").objectStore(collectionName).getAll()
 }
-const data = getAllData("Restaurant-Feedback")
 
-data.onsuccess = function(event){
-    let result = event.target.result
-    console.log(result)
-}
-// $(window).on('load', function() {
-//     let resultLoad = getAllData("Restaurant-Feedback")
-//     resultLoad.onsuccess = function(event) {
-//         let data = event.target.resultLoad;
-//         for(let i in data) {
-//             let newIndex = `<div class="col-md-3 col-sm-6">
-//                 <div class="products">
-//                 <div class="thumbnail"><a href="details.html"><img src="${data[i].image}" alt="Product Name"></a></div>
-//                 <div class="productname">${data[i].restaurant_name}</div>
-//                 <div class="button_group"><button class="button add-cart" type="button">Feedback Detail</button></div>
-//                 <div class="button_group"><button class="button add-cart" type="button">Feedback</button></div>
-//                 </div>
-//                 </div>`
-//             $('#data').append(newIndex);
-//         }
-//         console.log(data);
-//     }
-// })
+$(window).on('load', function() {
+    let resultLoad = getAllData("Restaurant-Feedback")
+    resultLoad.onsuccess = function(event) {
+        let result = event.target.result;
+        console.log(result)
+        for(let i in result) {
+
+            let newIndex = `<div class="col-md-3 col-sm-6">
+                <div class="products">
+                <div class="thumbnail"><a href="details.html"><img src="${result[i].image[0]}" alt="Product Name"></a></div>
+                <div class="productname">${result[i].restaurant_name}</div>
+                <div class="button_group"><button class="button add-cart" type="button">Feedback Detail</button></div>
+                <div class="button_group"><button class="button add-cart" type="button">Feedback</button></div>
+                </div>
+                </div>`
+            $('#data').append(newIndex);
+        }
+    }
+})
